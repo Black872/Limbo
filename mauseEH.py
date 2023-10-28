a921cd8cfe117a57d22b578bddca5aa3837ddaa9
enotjk <danila.19216@gmail.com>
2023-10-28 22:14:27 +0300

    input datetime

--- mauseEH.py
+++ mauseEH.py
@@ -1,24 +1,30 @@
+import time
 from pynput import mouse
+from datetime import datetime
 
 
 click_count = 0
 
 
-#функция, вызываемая при клике мыши
-def on_click(x, y, button, pressed):
-    global click_count
-    if pressed:
-        click_count += 1
-        print(f'Клик {click_count}')
+def handler():
+    #функция, вызываемая при клике мыши
+    def ClickHandler(x, y, button, pressed):
+        global click_count
+        time = datetime.now().time()
+        if pressed:
+            click_count += 1
+            print(f'Клик {click_count}, {time}')
 
-        if click_count >= 10:  #остановка прослушивания после 10 кликов
-            listener.stop()
+            if click_count >= 10:  #остановка отслеживание после 10 кликов
+                listener.stop()
+                l = False               
+                
+    #объект слушателя мыши
+    listener = mouse.Listener(on_click=ClickHandler)
+
             
-#объект слушателя мыши
-listener = mouse.Listener(on_click=on_click)
+    listener.start()
+    
+    listener.join()
 
 
-listener.start()
-
-
-listener.join()
